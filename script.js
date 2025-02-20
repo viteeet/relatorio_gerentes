a62aa5f81071499e279190a883fb46ea463d12ad
viteeet <victor.hg.pereira@gmail.com>
2025-02-20 12:42:43 -0300

    Update script.js
    colunas reordenadas

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ document.addEventListener("DOMContentLoaded", () => {
     let guiaAtual = "BORDEROS OPERADOS";
     let dados = {};
     const colunasVisiveis = {
-        "BORDEROS OPERADOS": ["data_oper", "bordero", "cedente", "empresa", "Valor Total", "Valor Liq. do", "Resultado Liquido", "tdesc", "pmpx", "Data Cadastro", "ntitulos", "gerente", "Semana mês", "DEPARA - GERENTE"],
-        "CARTEIRAS EM ABERTO": ["Vencimento", "Titulo", "sacado", "cedente", "Empresa", "situacao_titulo", "valor_face", "nr_bordero", "tipo_cobranca", "valor_titulo", "gerente"],
+        "BORDEROS OPERADOS": [ "gerente", "cedente", "data_oper", "bordero",  "empresa", "Valor Total", "Valor Liq. do", "Resultado Liquido", "tdesc", "pmpx", "Data Cadastro", "ntitulos", "Semana mês"],
+        "CARTEIRAS EM ABERTO": [ "gerente","cedente","Vencimento", "Titulo", "sacado",  "Empresa", "situacao_titulo", "valor_face", "nr_bordero", "tipo_cobranca", "valor_titulo"],
         "TITULOS QUITADOS": ["Gerente", "cedente", "Titulos", "venc0", "vencutil0", "quitacao", "valor", "mora", "total"],
-        "RISCO CEDENTE": ["gerente", "Cedente", "Limite", "Risco", "Tranche", "saldocc", "DEPARA - GERENTE", "Vencidos", "Valor corrigido", "Saldo p/ Operar", "A vencer"],
+        "RISCO CEDENTE": ["gerente", "Cedente", "Limite", "Risco", "Tranche", "saldocc", "Vencidos", "Valor corrigido", "Saldo p/ Operar", "A vencer"],
         "TITULOS VENCIDOS": ["GERENTE", "Cedente", "vencutil", "Vencimento", "Titulos", "SACADO_EMITENTE", "total", "VALOR_FACE", "VALOR_ATUAL", "VALOR_CORRIGIDO", "BANCO_COB", "DIAS_QTD"]
     };
 
